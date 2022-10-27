da91b51b8f0d103c95473917a67840d3d787a896
Md. Abdul Hadi <alhadi.cse@gmail.com>
2022-10-27 16:06:53 +0600

    ahi-doc-search-2

--- mowrknowledgerepo/urls.py
+++ mowrknowledgerepo/urls.py
@@ -1,20 +1,6 @@
-"""mowrknowledgerepo URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 admin.site.site_header = 'Knowledge Repository Admin Panel'
 admin.site.index_title = 'Admin'
@@ -26,4 +12,9 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     #path('', include('django.contrib.auth.urls')),
+
+    
+    path('search/', views.search_document, name="search"),
+    path('search/<search_term>', views.search_document, name="search"),
+    #path('search/<string:search_term>', views.search_document, name="search"),
 ]
